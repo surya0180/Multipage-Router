058b3166718a796be551c40ea8aa21163cdd6cf8
surya0180 <teja8551@gmail.com>
2021-07-09 18:58:28 +0530

    Added form logic

--- src/pages/NewQuote.jsx
+++ src/pages/NewQuote.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useHistory } from 'react-router-dom';
 import QuoteForm from '../components/quotes/QuoteForm';
 
 const NewQuotes = () => {
+    const history = useHistory();
+
     const addQuoteHandler = (quoteData) => {
         console.log(quoteData);
+
+        history.push('/quotes');
     };
+
 
     return(
         <QuoteForm onAddQuote={addQuoteHandler} isLoading={false} />
