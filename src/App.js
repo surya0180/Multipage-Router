bffaf8b88395d3a1518b8f37b1e5122cded74734
surya0180 <teja8551@gmail.com>
2021-07-09 16:47:27 +0530

    Added navigation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import { Route, Redirect, Switch } from 'react-router-dom'
-import AllQuotes from './pages/AllQuotes'
-import NewQuotes from './pages/NewQuote'
-import QuoteDetail from './pages/QuoteDetail'
+import { Route, Redirect, Switch } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import AllQuotes from "./pages/AllQuotes";
+import NewQuotes from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
 
 function App() {
   return (
     <div>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/quotes"/>
-        </Route>
-        <Route path="/quotes" exact>
-          <AllQuotes />
-        </Route>
-        <Route path="/quotes/:quoteId">
-          <QuoteDetail />
-        </Route>
-        <Route path="/new-quote" exact>
-          <NewQuotes />
-        </Route>
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="/quotes" exact>
+            <AllQuotes />
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetail />
+          </Route>
+          <Route path="/new-quote" exact>
+            <NewQuotes />
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
